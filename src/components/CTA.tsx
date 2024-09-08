e30d5d257813aeadffd42aefdf6db050bce3edef
Muhammad Rizwan <mhrizwan914@gmail.com>
2024-09-09 00:39:06 +0500

    Fix Animation

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -8,11 +8,11 @@ interface CTAProps {
 
 export default function CTA({ link = "javascript:;", text = "Get Started" }: CTAProps) {
     return (
-        <div
-            // initial={{ y: 0 }}
-            // whileHover={{ y: -5 }}
-            // transition={{ duration: 0.2 }}
-            // className="inline-block"
+        <motion.div
+            initial={{ y: 0 }}
+            whileHover={{ y: -5 }}
+            transition={{ duration: 0.2 }}
+            className="inline-block"
             dangerouslySetInnerHTML={{
                 __html: `<a href=${link} tabindex="0" class="py-3 px-5 primary-cta rounded-xl font-semibold text-[16px] text-black">
                     ${text}
